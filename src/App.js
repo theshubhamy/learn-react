2c632fcfaf9a7c4969deb3bde9c5e2c1ccf7ac03
shubham <devshubhamyadav@gmail.com>
2022-01-29 21:05:07 +0530

    shubham

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
 import React from "react";
 import Expense from "./containers/Expenses";
-import NewExpenses from "./components/newExpenses/NewExpenses";
 const App = () => {
-  const expense = [
-    {
-      id: "id-1",
-      title: "Toilet Paper",
-      amount: 98.12,
-      date: new Date(2022, 2, 24),
-    },
-    {
-      id: "id-2",
-      title: "News Paper",
-      amount: 98.12,
-      date: new Date(2022, 1, 20),
-    },
-    {
-      id: "id-3",
-      title: "car Insurance",
-      amount: 980.12,
-      date: new Date(2022, 1, 18),
-    },
-    {
-      id: "id-4",
-      title: "New Laptop",
-      amount: 988.12,
-      date: new Date(2022, 1, 16),
-    },
-    {
-      id: "id-5",
-      title: "New IPhone",
-      amount: 798.12,
-      date: new Date(2022, 1, 14),
-    },
-  ];
-  const addExpenseHandler = (expense) => {
-    console.log(expense);
-  };
   return (
     <>
-      <NewExpenses onAddExpense={addExpenseHandler} />
-      <Expense item={expense} />
+      <Expense />
     </>
   );
 };
--- src/containers/Expenses.jsx
+++ src/containers/Expenses.jsx
@@ -1,16 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "../components/expenses/ExpenseItem";
-
+import NewExpenses from "../components/newExpenses/NewExpenses";
+import ExpenseFilter from "../components/expenses/ExpenseFilter";
 import "./Expenses.css";
-const Expenses = (props) => {
-  const expense = props.item;
-  
+const Expenses = () => {
+  //const currentYear = new Date().getFullYear();
+  const [filteredYear, setFilteredYear] = useState("2022");
+  const initialExpense = [
+    {
+      id: "id-1",
+      title: "Toilet Paper",
+      amount: 98.12,
+      date: new Date(2023, 2, 24),
+    },
+    {
+      id: "id-2",
+      title: "News Paper",
+      amount: 98.12,
+      date: new Date(2022, 1, 20),
+    },
+    {
+      id: "id-3",
+      title: "car Insurance",
+      amount: 980.12,
+      date: new Date(2021, 1, 18),
+    },
+    {
+      id: "id-4",
+      title: "New Laptop",
+      amount: 988.12,
+      date: new Date(2020, 1, 16),
+    },
+    {
+      id: "id-5",
+      title: "New IPhone",
+      amount: 798.12,
+      date: new Date(2019, 1, 14),
+    },
+  ];
+  const [expenses, setExpenses] = useState(initialExpense);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filterExpenses = expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
   return (
     <>
-
+      <NewExpenses onAddExpense={addExpenseHandler} />
       <div className="expense">
+        <ExpenseFilter
+          selectedYear={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
         <>
-          {expense.map((expense) => (
+          {filterExpenses.map((expense) => (
             <ExpenseItem
               key={expense.id}
               title={expense.title}
